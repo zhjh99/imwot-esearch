fa7251d9ffd4cca2951eb0cb3a96d0f452822ea9
zhjh99 <zhoujinhong99@gmail.com>
2018-01-17 16:18:07 +0800

    no message

--- src/main/java/com/imwot/esearch/utils/FileUtils.java
+++ src/main/java/com/imwot/esearch/utils/FileUtils.java
@@ -1,121 +1,69 @@
 package com.imwot.esearch.utils;
 
-import java.io.BufferedOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.Date;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 
 public class FileUtils {
 
-	private int n = 10000000;
-
-	public void writeMethod2() {
-		String fileName = "c:/bit.dat";
+	public void writeMethod2(String fileName, byte[] datas, int start,
+			int length) {
 		try {
-			DataOutputStream out = new DataOutputStream(
-					new BufferedOutputStream(new FileOutputStream(fileName,true)));
 
-//			 out.writeUTF("100000000,9");
-//			 System.out.println(out.size() + " bytes have been written.2");
-			 
-			 // System.out.println(Long.MAX_VALUE +
-			 // " bytes have been written.2");
-
-			byte[]datas=new byte[1024];
-			int y=0;
-//			for (int x = 1; x <= n; x++) {
-//				String data=x + ",9";
-//				datas[y+x]=writeInt(data);
-//			}
-			out.write(datas);
-			out.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void readMethod2() {
-		try {
-			DataInputStream stream = new DataInputStream(new FileInputStream(
-					"c:/bit.dat"));
-			String docid;
-			while ((docid = stream.readUTF()) != null) {
-				stream.readUTF();
-//				System.out.println("docid:" + docid);
+			FileChannel fc = new RandomAccessFile(fileName, "rw").getChannel();
+			// 注意，文件通道的可读可写要建立在文件流本身可读写的基础之上
+			MappedByteBuffer out = fc.map(FileChannel.MapMode.READ_WRITE,
+					start, length);
+			// 写128M的内容
+			for (byte data : datas) {
+				out.put(data);
 			}
+
+			fc.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	private void writeO() throws Exception {
-		File file = new File("c:/bit2.dat");
-		FileOutputStream fos = new FileOutputStream(file);
-		ObjectOutputStream oos = new ObjectOutputStream(fos);
-
-		IndexList list = new IndexList();
-
-		MIndex mIndex2 = new MIndex();
-		mIndex2.setDocId(3);
-		mIndex2.setTf(1);
-		mIndex2.setPos("3");
-
-		MIndex mIndex3 = new MIndex();
-		mIndex3.setDocId(7);
-		mIndex3.setTf(1);
-		mIndex3.setPos("4");
-
-		for (int x = 1; x <= n; x++) {
-			MIndex mIndex1 = new MIndex();
-			mIndex1.setDocId(x);
-			mIndex1.setTf(9);
-			list.addPosIist(mIndex1);
+	public void readMethod2(String fileName, int length) {
+		try {
+			FileChannel fc = new RandomAccessFile(fileName, "rw").getChannel();
+			// 注意，文件通道的可读可写要建立在文件流本身可读写的基础之上
+			MappedByteBuffer out = fc.map(FileChannel.MapMode.READ_WRITE, 0,
+					length);
+			// 读取文件中间6个字节内容
+			for (int i = length - 1; i >= 0; i--) {
+				System.out.println((char) out.get(i));
+			}
+			fc.close();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		oos.writeObject(list);
-		oos.flush();
-		oos.close();
-		fos.close();
 	}
 
-	private void readO() throws Exception {
-		File file = new File("c:/bit2.dat");
-		FileInputStream fos = new FileInputStream(file);
-		ObjectInputStream oos = new ObjectInputStream(fos);
-
-		IndexList list = (IndexList) oos.readObject();
-		for (MIndex in : list.getPosIist()) {
-			System.out.println(in.getDocId());
-			System.out.println(in.getTf());
-		}
-		oos.close();
-		fos.close();
-	}
-	
 	public byte[] writeInt(int i) throws IOException {
-		byte[] data=new byte[4];
-		data[0]=(byte)(i >> 24);
-		data[0]=((byte)(i >> 16));
-		data[0]=((byte)(i >>  8));
-		data[0]=((byte) i);
+		byte[] data = new byte[4];
+		data[0] = (byte) (i >> 24);
+		data[0] = ((byte) (i >> 16));
+		data[0] = ((byte) (i >> 8));
+		data[0] = ((byte) i);
 		return data;
-	  }
+	}
 
 	public static void main(String[] str) throws Exception {
 		FileUtils fu = new FileUtils();
+		String fileName = "c:/bit.dat";
 
 		long ss1 = System.currentTimeMillis();
-		fu.writeMethod2();
+
+		byte[] bytes = new byte[] { 'd' };
+		fu.writeMethod2(fileName, bytes, 3, 1);
 		long ss2 = System.currentTimeMillis();
 		System.out.println(ss1 - ss1);
 
 		long ss3 = System.currentTimeMillis();
-		fu.readMethod2();
+		fu.readMethod2(fileName, 4);
 		long ss4 = System.currentTimeMillis();
 
 		System.out.println("字符串存储时间:" + (ss2 - ss1));
